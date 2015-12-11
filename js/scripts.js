b3b1c48e671e399b97fe3926c0fec0a57519fa0b
mgulova <gulovamarina@gmail.com>
2015-12-10 23:04:54 -0800

    IG plugin fixing up

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
-$('.instagram-lite').instagramLite({
-    clientID: '624415625',
-    username: 'tenthousandmoths'
-    
-    urls: true,
-    loadmore: '', //Add class, ID, or element ur using as a button to load more photos
-    
+$(function() {
+
+    $('.instagram-lite').instagramLite({
+        clientID: '624415625',
+        username: 'tenthousandmoths',
+        list: false,
+        urls: false,
+        load_more: '.load-more',
+        success: function() {
+            console.log('The request was successful!');
+        },
+        error: function(errorCode, errorMessage) {
+
+            console.log('There was an error with the request');
+
+            if(errorCode && errorMessage) {
+                console.log(errorCode +': '+ errorMessage);
+            }
+        }
+    });
+
 });
+
+
+
