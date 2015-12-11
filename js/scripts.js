2e78e07179bfff411149f36c06cf6ed023d0986a
mgulova <gulovamarina@gmail.com>
2015-12-11 00:16:10 -0800

    quitting any hope for the IG plugin

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,7 @@
-$(function() {
-
-    $('.instagram-lite').instagramLite({
+$('.instagram-lite').instagramLite({
         clientID: '624415625',
         username: 'tenthousandmoths',
         list: false,
         urls: false,
         load_more: '.load-more',
-        success: function() {
-            console.log('The request was successful!');
-        },
-        error: function(errorCode, errorMessage) {
-
-            console.log('There was an error with the request');
-
-            if(errorCode && errorMessage) {
-                console.log(errorCode +': '+ errorMessage);
-            }
-        }
-    });
-
-});
-
-
-
+ });
