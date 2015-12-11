14f275c5cc6489cc64524ebd17c4d90cb458a305
mgulova <gulovamarina@gmail.com>
2015-12-10 22:38:22 -0800

    IG plugin

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$('.instagram-lite').instagramLite({
+    clientID: '624415625',
+    username: 'tenthousandmoths'
+    
+    urls: true,
+    loadmore: '', //Add class, ID, or element ur using as a button to load more photos
+    
+});
